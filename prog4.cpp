a805741dc3e8b88ef609f8e834a680a71b877bcb
qwertyuiop5040 <hwang00@umail.ucsb.edu>
2017-06-07 22:01:17 -0700

    Add files via upload

--- prog4.cpp
+++ prog4.cpp
@@ -46,62 +46,44 @@ int main(){
 		ptsX.pb(x);
 		ptsY.pb(y);
 		positions.pb(i);
-		// cout<<ptsX[i]<<" "<<ptsY[i]<<endl;
 		errors.pb(0);
 		bestPositions.pb(i);
 		bestErrors.pb(0);
 	}
-	// according to lecture slides, we really only need 5 iterations for 95%, but we have greater confidence with more.
-	// with 100 iterations, we are almost completely sure that we picked good data at least once (assuming 30% outliers): 5.71e-30 probability to pick at least one outlier every time.
+	// according to lecture slides, we really only need 5 or 6 iterations for 95%, but we have greater confidence with more.
+	// with 100 iterations, we are almost completely sure that we picked good data at least once (assuming 30% outliers): only 5.71e-30 probability to pick at least one outlier every time.
 	double bestMedian=10e300;
 	double median;
 	double bestA,bestB;
-	int a1[5]={3,8,5,8,3};
-	int a2[5]={1,7,3,7,4};
-	forn(i,50){
+	forn(i,100){
 		int idx=rand()%n;
 		int idx2=rand()%n;
-		// int idx=a1[i];
-		// int idx2=a2[i];
 		while(idx2==idx)idx2=rand()%n;
-		// cout<<idx<<" "<<idx2<<endl;
 		double run=ptsX[idx]-ptsX[idx2];
 		if(abs(run)==0.0)run=1e-9;
 		double a=(ptsY[idx]-ptsY[idx2])/run;
 		double b=ptsY[idx]-a*ptsX[idx];//in form y=ax+b
-		// cout<<idx<<" "<<idx2<<" "<<ptsX[idx]<<" "<<ptsX[idx2]<<" "<<ptsY[idx]<<" "<<ptsY[idx2]<<" "<<a<<" "<<b<<endl;
-		// cout<<"error"<<endl;
 		forn(j,n){
 			double terr=(abs(ptsY[j]-(a*ptsX[j]+b)));
 			if(terr<1e-9)terr=0.0;
-			// cout<<j<<" "<<ptsY[j]<<" "<<a<<" "<<ptsX[j]<<" "<<b<<" "<<terr<<endl;
 			errors[j]=terr;
 		}
 		sort(positions.begin(),positions.end(),comp);
 		if(n%2==0){
-			// cout<<"g1";
-			 // forn(i,n){cout<<"DFS "<<errors[positions[i]]<<" ";}cout<<endl;
 			double leftMedian=errors[positions[n/2]];
-			// forn(i,n){cout<<"DFS "<<errors[positions[i]]<<" ";}cout<<endl;
 			median=(leftMedian+errors[positions[n/2-1]])/2.0;
-			// cout<<leftMedian<<" "<<errors[positions[n/2-1]]<<endl;
 		}else{
 			median=errors[positions[n/2]];
 		}
 		if(median<bestMedian){
-			// cout<<"NEW BEST"<<endl;
 			bestMedian=median;
-			// bestPositions.clear();
-			// bestErrors.clear();
-			// cout<<bestPositions.size()<<" "<<bestErrors.size()<<endl;
 			forn(j,n){
 				bestPositions[j]=(positions[j]);
 				bestErrors[j]=(errors[j]);
 			}
 		}
-		// cout<<"median: "<<median<<" "<<bestMedian<<endl;
 	}
-	int lastIndexLessThanMedian=n/2-1;
+	int lastIndexLessThanMedian=n/2-1;//0..lastIndex are used in the regression
 	double sumX=0,sumXSq=0,sumXY=0,sumY=0,sum1=lastIndexLessThanMedian+1;
 	sort(bestPositions.begin(),bestPositions.end(),comp2);
 	forn(i,lastIndexLessThanMedian+1){
